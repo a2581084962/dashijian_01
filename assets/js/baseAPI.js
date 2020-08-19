9e5b6318d78c23bae78d6626d25fc885b29a02e5
wentao <2581084962@qq.com>
2020-08-19 14:49:55 +0800

    主页功能完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -11,4 +11,25 @@ $.ajaxPrefilter(function (params) {
     // 拼接服务器地址
     params.url = baseURL + params.url;
     // alert(params.url)
+
+    // 2.对需要权限的接口配置头信息
+    // 必须以my开头才行
+    if (params.url.indexOf('/my/') !== -1) {
+        params.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    // 3.拦截所有的响应，判断身份认证的信息
+    params.complete = function (res) {
+        console.log(res.responseJSON);
+        var obj = res.responseJSON;
+        // 身份认证失败的字体后面的！是中文感叹号。
+        if (obj.status == 1 && obj.message == '身份认证失败！') {
+            // 清空本地token
+            localStorage.removeItem('token');
+            // 2.页面跳转
+            location.href = '/login.html'
+        }
+    }
 })
